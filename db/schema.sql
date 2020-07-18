c99e05fdb8c4e4487f6c578753591c73bc15c76c
thetntm <michael.becker.123@gmail.com>
2020-07-18 15:13:31 -0700

    followed the rest of the instructions, forgot to commit often.

---
+++ db/schema.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE IF NOT EXISTS my_burgers_db;
+USE my_burgers_db;
+
+-- If the table already exists, remove it before trying to create the table again
+DROP TABLE IF EXISTS burgers;
+
+-- Create the burgers table
+CREATE TABLE burgers (
+    id int NOT NULL AUTO_INCREMENT,
+    burger_name varchar(255) NOT NULL,
+    devoured BOOL DEFAULT false,
+    PRIMARY KEY (id)
+);
